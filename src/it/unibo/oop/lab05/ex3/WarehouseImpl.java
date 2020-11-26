bdbdf55992937adcecf472ac0dcb54a7b4597bc4
Giammuzz <giammi53@hotmail.com>
2020-11-26 23:15:16 +0100

    aggiunti 2 file

--- src/it/unibo/oop/lab05/ex3/WarehouseImpl.java
+++ src/it/unibo/oop/lab05/ex3/WarehouseImpl.java
@@ -1,6 +1,5 @@
 package it.unibo.oop.lab05.ex3;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
--- src/it/unibo/oop/lab05/ex4/UseWarehouse.java
+++ src/it/unibo/oop/lab05/ex4/UseWarehouse.java
@@ -28,10 +28,10 @@ public final class UseWarehouse {
          * 
          * 2) The implementation of Product must implement Comparable<Product>.
          */
-        final Product p1 = null;
-        final Product p2 = null;
-        final Product p3 = null;
-        final Warehouse w = null;
+        final Product p1 = new ComparableProduct("ap1", 89.2);
+        final Product p2 = new ComparableProduct("bp2", 44);
+        final Product p3 = new ComparableProduct("cp3", 34);
+        final Warehouse w = new OrderedWarehouse();
         w.addProduct(p3);
         w.addProduct(p1);
         w.addProduct(p2);
